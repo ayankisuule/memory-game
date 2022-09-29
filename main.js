938924b2f8377e91945526d2b7b5f1059b4ddd0f
ozzzy kyebuzibwa <ayankisuule@gmail.com>
2022-09-29 13:43:03 -0400

    added form

---
+++ main.js
@@ -0,0 +1,13 @@
+const generateMemeBtn =document.querySelector(".meme-generator .generate-meme-btn");
+
+const memeImage = docoment.querySelector(".meme-generator img");
+
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+
+const memeAuthor = document.querySelector(".meme-generator .meme-author");
+
+const generateMeme = () => {
+    
+}
+
+generateMemeBtn.addEventListener("click", generateMeme);
